a03117b0b88ae3659d110700828d0ffd34f89145
Arthur Ydalgo <25585428+ArthurYdalgo@users.noreply.github.com>
2026-07-05 06:35:28 -0300

    Update routes.php

--- lang/pt/routes.php
+++ lang/pt/routes.php
@@ -1,8 +1,24 @@
 <?php
 
 return [
+    'dashboard' => 'painel',
+    'forgot-password' => 'esqueci-a-senha',
+    
+    'login' => 'entrar',
+
+    'reset-password/{token}' => 'redefinir-senha/{token}',
+    'register' => 'registrar',
+
     'settings/appearance' => 'configuracoes/aparencia',
+    'settings/language' => 'configuracoes/idioma',
+    'settings/profile' => 'configuracoes/perfil',
+    'settings/password' => 'configuracoes/senha',
+    'settings' => 'configuracoes',
+
+    
     'users' => 'usuarios',
     'users/{user}' => 'usuarios/{user}',
     'users/{user}/edit' => 'usuarios/{user}/editar',
+
+    'verify-email' => 'verificar-email',
 ];
